106088de3f8720499195cc37ff5e5369d95cb380
Wasiue03 <channarwasiue@yahoo.com>
2024-06-27 12:29:22 +0500

    modified name

--- src/Common.jsx
+++ src/Common.jsx
@@ -13,7 +13,7 @@ const Common = (props) => {
                 <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column">
                   <h1>
                     {props.name}
-                    <strong className="brand-name" style={{fontSize: "20px"}}> <br />Data Science & Software Development </strong>
+                    <strong className="brand-name" style={{fontSize: "20px"}}> <br />Data Scientist </strong>
                   </h1>
                   <h2 className="my-3" style={{color: "#fff", justifyContent:'flex-start'}}>
                     Wasiue is <strong>Data Scientist</strong> with more than 3 years expeirence of Software Development, Machine Learning Engineering and Data Science.
